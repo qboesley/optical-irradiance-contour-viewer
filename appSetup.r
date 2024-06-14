95d1e9a1e12365da44456d5593d088d14a61d7a8
qboesley <127060519+qboesley@users.noreply.github.com>
2024-06-14 15:23:31 +1200

    Add files via upload

--- appSetup.r
+++ appSetup.r
@@ -1,23 +1,26 @@
 # Create folders to download app components to. The folder 'OptoContourViewerApp' will be created in your R working directory.
-cat("Creating folders for app components")
+cat("Creating folders for app components\n")
 dir.create(file.path('OptoContourViewerApp', 'data'), recursive = TRUE)
 
 # download app.R file into directory
 #update the url and destfile to access the local version of the app
 cat("Downloading app file\n")
-download.file("https://github.com/BrainStimulation/optical-irradiance-contour-viewer/blob/main/src/app.R?raw=1", destfile = file.path('OptoContourViewerApp', 'app.R'))
+download.file("https://github.com/qboesley/optical-irradiance-contour-viewer/blob/main/localApp.r?raw=1", destfile = file.path('OptoContourViewerApp', 'localApp.R'))
 
 # download white and grey matter data files into the data directory
-cat("Downloading data files\n")
+cat("Downloading data files:\n")
+cat("grey.mat\n")
 download.file("https://brainstimulation.github.io/optical-irradiance-contour-webapp/data/grey.mat?raw=1", destfile = file.path('OptoContourViewerApp', 'data', 'grey.mat'), quiet = TRUE, mode = 'wb')
+cat("white.mat\n")
 download.file("https://brainstimulation.github.io/optical-irradiance-contour-webapp/data/white.mat?raw=1", destfile = file.path('OptoContourViewerApp', 'data', 'white.mat'), quiet = TRUE, mode = 'wb')
 
 # install required packages
+# https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
 cat("Installing necessary packages\n")
-install.packages("shiny", repos = "http://cran.us.r-project.org")
-install.packages("bslib", repos = "http://cran.us.r-project.org")
-install.packages("R.matlab", repos = "http://cran.us.r-project.org")
+requiredpackages <- c("shiny", "bslib", "R.matlab")
+notyetinstalled <- requiredpackages[!(requiredpackages %in% requiredpackages()[,"Package"])]
+if(length(notyetinstalled)) install.packages(notyetinstalled, repos = "http://cran.us.r-project.org")
 
 # exit message
-cat("All app components downloaded! To start the app run the command below in this console:\n\nsource(\"localApp.R\")")
+cat("All app components downloaded! To start the app run the command below in this console:\n\nsource(file.path(\"OptoContourViewerApp\", \"localApp.R\"))")
 
